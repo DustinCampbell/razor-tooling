8472fdfdde1f74b90e0661fcd0e4a3e95cc53c25
Dustin Campbell <dustin@teamcampbell.org>
2024-07-03 10:02:28 -0700

    Clean up SplatTagHelperDescriptorProvider

--- src/Compiler/Microsoft.CodeAnalysis.Razor.Compiler/src/CSharp/SplatTagHelperDescriptorProvider.cs
+++ src/Compiler/Microsoft.CodeAnalysis.Razor.Compiler/src/CSharp/SplatTagHelperDescriptorProvider.cs
@@ -1,9 +1,8 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-#nullable disable
-
 using System;
+using Microsoft.AspNetCore.Razor;
 using Microsoft.AspNetCore.Razor.Language;
 using Microsoft.AspNetCore.Razor.Language.Components;
 using static Microsoft.AspNetCore.Razor.Language.CommonMetadata;
@@ -12,19 +11,16 @@ namespace Microsoft.CodeAnalysis.Razor;
 
 internal class SplatTagHelperDescriptorProvider : ITagHelperDescriptorProvider
 {
-    private static TagHelperDescriptor s_splatTagHelper;
+    private static readonly Lazy<TagHelperDescriptor> s_splatTagHelper = new(CreateSplatTagHelper);
 
     // Order doesn't matter
     public int Order { get; set; }
 
-    public RazorEngine Engine { get; set; }
+    public RazorEngine? Engine { get; set; }
 
     public void Execute(TagHelperDescriptorProviderContext context)
     {
-        if (context == null)
-        {
-            throw new ArgumentNullException(nameof(context));
-        }
+        ArgHelper.ThrowIfNull(context);
 
         var compilation = context.Compilation;
 
@@ -42,50 +38,45 @@ internal class SplatTagHelperDescriptorProvider : ITagHelperDescriptorProvider
             return;
         }
 
-        context.Results.Add(GetOrCreateSplatTagHelper());
+        context.Results.Add(s_splatTagHelper.Value);
     }
 
-    private static TagHelperDescriptor GetOrCreateSplatTagHelper()
+    private static TagHelperDescriptor CreateSplatTagHelper()
     {
-        return s_splatTagHelper ??= CreateSplatTagHelper();
+        using var _ = TagHelperDescriptorBuilder.GetPooledInstance(
+            ComponentMetadata.Splat.TagHelperKind, "Attributes", ComponentsApi.AssemblyName,
+            out var builder);
 
-        static TagHelperDescriptor CreateSplatTagHelper()
+        builder.CaseSensitive = true;
+        builder.SetDocumentation(DocumentationDescriptor.SplatTagHelper);
+
+        builder.SetMetadata(
+            SpecialKind(ComponentMetadata.Splat.TagHelperKind),
+            MakeTrue(TagHelperMetadata.Common.ClassifyAttributesOnly),
+            RuntimeName(ComponentMetadata.Splat.RuntimeName),
+            TypeName("Microsoft.AspNetCore.Components.Attributes"));
+
+        builder.TagMatchingRule(rule =>
         {
-            using var _ = TagHelperDescriptorBuilder.GetPooledInstance(
-                ComponentMetadata.Splat.TagHelperKind, "Attributes", ComponentsApi.AssemblyName,
-                out var builder);
-
-            builder.CaseSensitive = true;
-            builder.SetDocumentation(DocumentationDescriptor.SplatTagHelper);
-
-            builder.SetMetadata(
-                SpecialKind(ComponentMetadata.Splat.TagHelperKind),
-                MakeTrue(TagHelperMetadata.Common.ClassifyAttributesOnly),
-                RuntimeName(ComponentMetadata.Splat.RuntimeName),
-                TypeName("Microsoft.AspNetCore.Components.Attributes"));
-
-            builder.TagMatchingRule(rule =>
+            rule.TagName = "*";
+            rule.Attribute(attribute =>
             {
-                rule.TagName = "*";
-                rule.Attribute(attribute =>
-                {
-                    attribute.Name = "@attributes";
-                    attribute.SetMetadata(Attributes.IsDirectiveAttribute);
-                });
-            });
-
-            builder.BindAttribute(attribute =>
-            {
-                attribute.SetDocumentation(DocumentationDescriptor.SplatTagHelper);
                 attribute.Name = "@attributes";
-
-                attribute.TypeName = typeof(object).FullName;
-                attribute.SetMetadata(
-                    PropertyName("Attributes"),
-                    IsDirectiveAttribute);
+                attribute.SetMetadata(Attributes.IsDirectiveAttribute);
             });
+        });
 
-            return builder.Build();
-        }
+        builder.BindAttribute(attribute =>
+        {
+            attribute.SetDocumentation(DocumentationDescriptor.SplatTagHelper);
+            attribute.Name = "@attributes";
+
+            attribute.TypeName = typeof(object).FullName;
+            attribute.SetMetadata(
+                PropertyName("Attributes"),
+                IsDirectiveAttribute);
+        });
+
+        return builder.Build();
     }
 }
