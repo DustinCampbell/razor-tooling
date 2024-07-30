c7898c9cae22240a07fbd5318d4ca10797127eb4
Dustin Campbell <dustin@teamcampbell.org>
2024-07-30 11:45:02 -0700

    Don't create List in CompilationTagHelperFeature unless needed

--- src/Compiler/Microsoft.CodeAnalysis.Razor.Compiler/src/CSharp/CompilationTagHelperFeature.cs
+++ src/Compiler/Microsoft.CodeAnalysis.Razor.Compiler/src/CSharp/CompilationTagHelperFeature.cs
@@ -18,14 +18,13 @@ public sealed class CompilationTagHelperFeature : RazorEngineFeatureBase, ITagHe
 
     public IReadOnlyList<TagHelperDescriptor> GetDescriptors()
     {
-        var results = new List<TagHelperDescriptor>();
-
         var compilation = CSharpCompilation.Create("__TagHelpers", references: _referenceFeature.References);
         if (!IsValidCompilation(compilation))
         {
             return [];
         }
 
+        var results = new List<TagHelperDescriptor>();
         var context = new TagHelperDescriptorProviderContext(compilation, results);
 
         for (var i = 0; i < _providers.Length; i++)
