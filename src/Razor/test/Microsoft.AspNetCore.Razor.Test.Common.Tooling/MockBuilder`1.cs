a0d6fc87c4d3a8e09a9397fa25405683ef9f61de
Dustin Campbell <dustin@teamcampbell.org>
2024-08-20 13:25:54 -0700

    Introduce simple IMockBuilder<T> abstraction for tests

---
+++ src/Razor/test/Microsoft.AspNetCore.Razor.Test.Common.Tooling/MockBuilder`1.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT license. See License.txt in the project root for license information.
+
+namespace Microsoft.AspNetCore.Razor.Test.Common;
+
+internal abstract class MockBuilder<T> : IMockBuilder<T>
+    where T : class
+{
+    public StrictMock<T> Mock { get; }
+
+    protected MockBuilder()
+    {
+        Mock = new();
+    }
+}
+
+internal interface IMockBuilder<T>
+    where T : class
+{
+}
