0f28950df5a2fe8a999917851d1f41555868a858
Dustin Campbell <dustin@teamcampbell.org>
2024-08-20 17:12:02 -0700

    Add a couple of Moq helper extension methods for returning null.

---
+++ src/Razor/test/Microsoft.AspNetCore.Razor.Test.Common.Tooling/MoqExtensions.cs
@@ -0,0 +1,30 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT license. See License.txt in the project root for license information.
+
+using Moq.Language;
+using Moq.Language.Flow;
+
+namespace Microsoft.AspNetCore.Razor.Test.Common;
+
+internal static class MoqExtensions
+{
+    public static IReturnsResult<T> ReturnsNull<T, TResult>(this IReturns<T, TResult?> returns)
+        where T : class
+        where TResult : class
+    {
+        return returns.Returns((TResult?)null);
+    }
+
+    public static IReturnsResult<T> ReturnsNull<T, TResult>(this IReturns<T, TResult?> returns)
+        where T : class
+        where TResult : struct
+    {
+        return returns.Returns((TResult?)null);
+    }
+
+    public static IReturnsResult<T> ReturnsValue<T, TResult>(this IReturns<T, TResult?> returns, TResult value)
+        where T : class
+    {
+        return returns.Returns(value: value);
+    }
+}
