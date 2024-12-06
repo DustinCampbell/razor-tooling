c4b58d7f4c778e01e1ba4ebd56924cc6ee9dc04b
Dustin Campbell <dustin@teamcampbell.org>
2024-12-05 16:59:28 -0800

    Clean up ProjectSnapshotTest

--- src/Razor/test/Microsoft.CodeAnalysis.Razor.Workspaces.Test/ProjectSystem/ProjectSnapshotTest.cs
+++ src/Razor/test/Microsoft.CodeAnalysis.Razor.Workspaces.Test/ProjectSystem/ProjectSnapshotTest.cs
@@ -1,9 +1,9 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the MIT license. See License.txt in the project root for license information.
 
-using System.Collections.Immutable;
 using System.Linq;
 using Microsoft.AspNetCore.Razor.Language;
+using Microsoft.AspNetCore.Razor.ProjectEngineHost;
 using Microsoft.AspNetCore.Razor.ProjectSystem;
 using Microsoft.AspNetCore.Razor.Test.Common;
 using Microsoft.AspNetCore.Razor.Test.Common.Workspaces;
@@ -12,68 +12,59 @@ using Xunit.Abstractions;
 
 namespace Microsoft.CodeAnalysis.Razor.ProjectSystem;
 
-public class DefaultProjectSnapshotTest : WorkspaceTestBase
+public class ProjectSnapshotTest(ITestOutputHelper testOutput) : ToolingTestBase(testOutput)
 {
-    private readonly HostDocument[] _documents;
-    private readonly HostProject _hostProject;
-    private readonly ProjectWorkspaceState _projectWorkspaceState;
+    private static readonly HostProject s_hostProject = TestProjectData.SomeProject with { Configuration = FallbackRazorConfiguration.MVC_2_0 };
+    private static readonly ProjectWorkspaceState s_projectWorkspaceState = ProjectWorkspaceState.Create([TagHelperDescriptorBuilder.Create("TestTagHelper", "TestAssembly").Build()]);
 
-    public DefaultProjectSnapshotTest(ITestOutputHelper testOutput)
-        : base(testOutput)
-    {
-        _hostProject = TestProjectData.SomeProject with { Configuration = FallbackRazorConfiguration.MVC_2_0 };
-        _projectWorkspaceState = ProjectWorkspaceState.Create(ImmutableArray.Create(
-            TagHelperDescriptorBuilder.Create("TestTagHelper", "TestAssembly").Build()));
+    private static readonly HostDocument[] s_documents =
+    [
+        TestProjectData.SomeProjectFile1,
+        TestProjectData.SomeProjectFile2,
 
-        _documents =
-        [
-            TestProjectData.SomeProjectFile1,
-            TestProjectData.SomeProjectFile2,
-
-            // linked file
-            TestProjectData.AnotherProjectNestedFile3,
-        ];
-    }
-
-    protected override void ConfigureProjectEngine(RazorProjectEngineBuilder builder)
-    {
-        builder.SetImportFeature(new TestImportProjectFeature());
-    }
+        // linked file
+        TestProjectData.AnotherProjectNestedFile3
+    ];
 
     [Fact]
     public void ProjectSnapshot_CachesDocumentSnapshots()
     {
         // Arrange
-        var state = ProjectState.Create(ProjectEngineFactoryProvider, LanguageServerFeatureOptions, _hostProject, _projectWorkspaceState)
-            .WithAddedHostDocument(_documents[0], EmptyTextLoader.Instance)
-            .WithAddedHostDocument(_documents[1], EmptyTextLoader.Instance)
-            .WithAddedHostDocument(_documents[2], EmptyTextLoader.Instance);
-        var snapshot = new ProjectSnapshot(state);
+        var state = ProjectState
+            .Create(ProjectEngineFactories.DefaultProvider, TestLanguageServerFeatureOptions.Instance, s_hostProject, s_projectWorkspaceState)
+            .WithAddedHostDocument(s_documents[0], EmptyTextLoader.Instance)
+            .WithAddedHostDocument(s_documents[1], EmptyTextLoader.Instance)
+            .WithAddedHostDocument(s_documents[2], EmptyTextLoader.Instance);
+
+        var project = new ProjectSnapshot(state);
 
         // Act
-        var documents = snapshot.DocumentFilePaths.ToDictionary(f => f, f => snapshot.GetDocument(f));
+        var documents = project.DocumentFilePaths
+            .Select(filePath => (filePath, document: project.GetDocument(filePath)));
 
         // Assert
         Assert.Collection(
             documents,
-            d => Assert.Same(d.Value, snapshot.GetDocument(d.Key)),
-            d => Assert.Same(d.Value, snapshot.GetDocument(d.Key)),
-            d => Assert.Same(d.Value, snapshot.GetDocument(d.Key)));
+            t => Assert.Same(t.document, project.GetDocument(t.filePath)),
+            t => Assert.Same(t.document, project.GetDocument(t.filePath)),
+            t => Assert.Same(t.document, project.GetDocument(t.filePath)));
     }
 
     [Fact]
     public void GetRelatedDocuments_NonImportDocument_ReturnsEmpty()
     {
         // Arrange
-        var state = ProjectState.Create(ProjectEngineFactoryProvider, LanguageServerFeatureOptions, _hostProject, _projectWorkspaceState)
-            .WithAddedHostDocument(_documents[0], EmptyTextLoader.Instance);
-        var snapshot = new ProjectSnapshot(state);
+        var state = ProjectState
+            .Create(ProjectEngineFactories.DefaultProvider, TestLanguageServerFeatureOptions.Instance, s_hostProject, s_projectWorkspaceState)
+            .WithAddedHostDocument(s_documents[0], EmptyTextLoader.Instance);
 
-        var document = snapshot.GetDocument(_documents[0].FilePath);
+        var project = new ProjectSnapshot(state);
+
+        var document = project.GetDocument(s_documents[0].FilePath);
         Assert.NotNull(document);
 
         // Act
-        var documents = snapshot.GetRelatedDocuments(document);
+        var documents = project.GetRelatedDocuments(document);
 
         // Assert
         Assert.Empty(documents);
@@ -83,22 +74,24 @@ public class DefaultProjectSnapshotTest : WorkspaceTestBase
     public void GetRelatedDocuments_ImportDocument_ReturnsRelated()
     {
         // Arrange
-        var state = ProjectState.Create(ProjectEngineFactoryProvider, LanguageServerFeatureOptions, _hostProject, _projectWorkspaceState)
-            .WithAddedHostDocument(_documents[0], EmptyTextLoader.Instance)
-            .WithAddedHostDocument(_documents[1], EmptyTextLoader.Instance)
+        var state = ProjectState
+            .Create(ProjectEngineFactories.DefaultProvider, TestLanguageServerFeatureOptions.Instance, s_hostProject, s_projectWorkspaceState)
+            .WithAddedHostDocument(s_documents[0], EmptyTextLoader.Instance)
+            .WithAddedHostDocument(s_documents[1], EmptyTextLoader.Instance)
             .WithAddedHostDocument(TestProjectData.SomeProjectImportFile, EmptyTextLoader.Instance);
-        var snapshot = new ProjectSnapshot(state);
 
-        var document = snapshot.GetDocument(TestProjectData.SomeProjectImportFile.FilePath);
+        var project = new ProjectSnapshot(state);
+
+        var document = project.GetDocument(TestProjectData.SomeProjectImportFile.FilePath);
         Assert.NotNull(document);
 
         // Act
-        var documents = snapshot.GetRelatedDocuments(document);
+        var documents = project.GetRelatedDocuments(document);
 
         // Assert
         Assert.Collection(
-            documents.OrderBy(d => d.FilePath),
-            d => Assert.Equal(_documents[0].FilePath, d.FilePath),
-            d => Assert.Equal(_documents[1].FilePath, d.FilePath));
+            documents.OrderBy(static d => d.FilePath),
+            d => Assert.Equal(s_documents[0].FilePath, d.FilePath),
+            d => Assert.Equal(s_documents[1].FilePath, d.FilePath));
     }
 }
