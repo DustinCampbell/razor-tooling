c0c118a8693af3d1a86ed588ac6756d78875fc24
Dustin Campbell <dustin@teamcampbell.org>
2024-12-09 15:32:24 -0800

    Improve performance and correctness of ProjectWorkspaceState equality

--- src/Razor/src/Microsoft.AspNetCore.Razor.ProjectEngineHost/ProjectSystem/ProjectWorkspaceState.cs
+++ src/Razor/src/Microsoft.AspNetCore.Razor.ProjectEngineHost/ProjectSystem/ProjectWorkspaceState.cs
@@ -3,7 +3,6 @@
 
 using System;
 using System.Collections.Immutable;
-using System.Linq;
 using Microsoft.AspNetCore.Razor.Language;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.Extensions.Internal;
@@ -12,7 +11,7 @@ namespace Microsoft.AspNetCore.Razor.ProjectSystem;
 
 internal sealed class ProjectWorkspaceState : IEquatable<ProjectWorkspaceState>
 {
-    public static readonly ProjectWorkspaceState Default = new(ImmutableArray<TagHelperDescriptor>.Empty, LanguageVersion.Default);
+    public static readonly ProjectWorkspaceState Default = new(tagHelpers: [], LanguageVersion.Default);
 
     public ImmutableArray<TagHelperDescriptor> TagHelpers { get; }
     public LanguageVersion CSharpLanguageVersion { get; }
@@ -21,29 +20,66 @@ internal sealed class ProjectWorkspaceState : IEquatable<ProjectWorkspaceState>
         ImmutableArray<TagHelperDescriptor> tagHelpers,
         LanguageVersion csharpLanguageVersion)
     {
-        TagHelpers = tagHelpers;
+        TagHelpers = tagHelpers.NullToEmpty();
         CSharpLanguageVersion = csharpLanguageVersion;
     }
 
     public static ProjectWorkspaceState Create(
         ImmutableArray<TagHelperDescriptor> tagHelpers,
         LanguageVersion csharpLanguageVersion = LanguageVersion.Default)
-        => tagHelpers.IsEmpty && csharpLanguageVersion == LanguageVersion.Default
+        => tagHelpers.IsDefaultOrEmpty && csharpLanguageVersion == LanguageVersion.Default
             ? Default
             : new(tagHelpers, csharpLanguageVersion);
 
     public static ProjectWorkspaceState Create(LanguageVersion csharpLanguageVersion)
         => csharpLanguageVersion == LanguageVersion.Default
             ? Default
-            : new(ImmutableArray<TagHelperDescriptor>.Empty, csharpLanguageVersion);
+            : new(tagHelpers: [], csharpLanguageVersion);
 
     public override bool Equals(object? obj)
         => obj is ProjectWorkspaceState other && Equals(other);
 
     public bool Equals(ProjectWorkspaceState? other)
-        => other is not null &&
-           TagHelpers.SequenceEqual(other.TagHelpers) &&
-           CSharpLanguageVersion == other.CSharpLanguageVersion;
+    {
+        if (ReferenceEquals(this, other))
+        {
+            return true;
+        }
+
+        if (other is null ||
+            CSharpLanguageVersion != other.CSharpLanguageVersion)
+        {
+            return false;
+        }
+
+        // PERF: Dig into the underlying arrays of TagHelpers to perform more efficient checks.
+        var span = TagHelpers.AsSpan();
+        var otherSpan = other.TagHelpers.AsSpan();
+
+        // Returns true if the spans point to the same memory and their lengths are the same.
+        if (span == otherSpan)
+        {
+            return true;
+        }
+
+        if (span.Length != otherSpan.Length)
+        {
+            return false;
+        }
+
+        while (span is [var item, ..] && otherSpan is [var otherItem, ..])
+        {
+            if (!ReferenceEquals(item, otherItem) && !item.Equals(otherItem))
+            {
+                return false;
+            }
+
+            span = span[1..];
+            otherSpan = otherSpan[1..];
+        }
+
+        return true;
+    }
 
     public override int GetHashCode()
     {
