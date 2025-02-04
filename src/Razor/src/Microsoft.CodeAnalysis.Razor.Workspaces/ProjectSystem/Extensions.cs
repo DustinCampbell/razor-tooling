e10fe2edadff0eec2e634882d422ebfa5d948469
Dustin Campbell <dustin@teamcampbell.org>
2025-02-04 10:45:41 -0800

    Add helper to convert RazorProjectitem.FilePath to target path

--- src/Razor/src/Microsoft.CodeAnalysis.Razor.Workspaces/ProjectSystem/Extensions.cs
+++ src/Razor/src/Microsoft.CodeAnalysis.Razor.Workspaces/ProjectSystem/Extensions.cs
@@ -1,11 +1,10 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the MIT license. See License.txt in the project root for license information.
 
-#if !NET
 using System;
-#endif
-
 using System.Diagnostics;
+using Microsoft.AspNetCore.Razor;
+using Microsoft.AspNetCore.Razor.Language;
 using Microsoft.AspNetCore.Razor.ProjectSystem;
 using Microsoft.AspNetCore.Razor.Serialization;
 using Microsoft.AspNetCore.Razor.Utilities;
@@ -37,5 +36,69 @@ internal static class Extensions
         Debug.Assert(projectKey.Id.EndsWith('/'), $"This method can't be called if {nameof(projectKey.Id)} is not a normalized directory path.");
 
         return FilePathNormalizer.AreDirectoryPathsEquivalent(projectKey.Id, project.CompilationOutputInfo.AssemblyPath);
+    }
+
+    /// <summary>
+    ///  Computes a Razor target path (e.g. 'Components\Pages\Error.razor') from a <see cref="RazorProjectItem.FilePath"/>.
+    /// </summary>
+    /// 
+    /// <remarks>
+    ///  <see cref="RazorProjectItem.FilePath "/> is defined as relative to the project root with a leading '/'
+    ///  and all other slashes normalized to '/'.
+    /// </remarks>
+    public static string? GetTargetPathFromFilePath(this RazorProjectItem projectItem)
+    {
+        var filePath = projectItem.FilePath;
+
+        // Some RazorProjectItems have a null FilePath, such as default imports.
+        if (filePath is null)
+        {
+            return null;
+        }
+
+        var length = filePath.Length;
+        var startIndex = 0;
+
+        // RazorProjectItem.FilePath *should* start with a '/', but we'll check just in case.
+        if (filePath.StartsWith('/'))
+        {
+            startIndex++;
+            length--;
+        }
+
+        // Is there nothing left? If so, skip it.
+        if (length == 0)
+        {
+            return null;
+        }
+
+        return StringFactory.Create(length, state: (filePath, startIndex), static (span, state) =>
+        {
+            var filePath = state.filePath.AsSpan(state.startIndex);
+
+            while (!filePath.IsEmpty)
+            {
+                // Find the next slash.
+                var slashIndex = filePath.IndexOf('/');
+
+                // If there aren't anymore slashes, copy the remaining file path.
+                if (slashIndex < 0)
+                {
+                    filePath.CopyTo(span);
+                    span = span[filePath.Length..];
+                    break;
+                }
+
+                filePath[..slashIndex].CopyTo(span);
+
+                filePath = filePath[(slashIndex + 1)..];
+                span = span[slashIndex..];
+
+                span[0] = '\\';
+                span = span[1..];
+            }
+
+            Debug.Assert(span.IsEmpty);
+        });
     }
 }
---
+++ src/Razor/test/Microsoft.CodeAnalysis.Razor.Workspaces.Test/ProjectSystem/ExtensionTest.cs
@@ -0,0 +1,58 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT license. See License.txt in the project root for license information.
+
+using Microsoft.AspNetCore.Razor.Language;
+using Microsoft.AspNetCore.Razor.Test.Common;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Microsoft.CodeAnalysis.Razor.ProjectSystem;
+
+public class ExtensionTest(ITestOutputHelper testOutput) : ToolingTestBase(testOutput)
+{
+    [Theory]
+    [InlineData("x", "x")]
+    [InlineData("_Imports.razor", "_Imports.razor")]
+    [InlineData("/_Imports.razor", "_Imports.razor")]
+    [InlineData("Views/_Imports.razor", @"Views\_Imports.razor")]
+    [InlineData("/Views/_Imports.razor", @"Views\_Imports.razor")]
+    [InlineData("Components/Pages/Error.razor", @"Components\Pages\Error.razor")]
+    [InlineData("/Components/Pages/Error.razor", @"Components\Pages\Error.razor")]
+    public void GetTargetPathFromFilePath(string filePath, string expectedTargetPath)
+    {
+        // Arrange
+        var projectItem = new TestRazorProjectItem(filePath);
+
+        // Act
+        var targetPath = projectItem.GetTargetPathFromFilePath();
+
+        // Assert
+        Assert.Equal(expectedTargetPath, targetPath);
+    }
+
+    [Fact]
+    public void GetTargetPathFromFilePath_HandlesNull()
+    {
+        // Arrange
+        var projectItem = new TestRazorProjectItem(filePath: null!);
+
+        // Act
+        var targetPath = projectItem.GetTargetPathFromFilePath();
+
+        // Assert
+        Assert.Null(targetPath);
+    }
+
+    [Fact]
+    public void GetTargetPathFromFilePath_HandlesSingleSlash()
+    {
+        // Arrange
+        var projectItem = new TestRazorProjectItem(filePath: "/");
+
+        // Act
+        var targetPath = projectItem.GetTargetPathFromFilePath();
+
+        // Assert
+        Assert.Null(targetPath);
+    }
+}
